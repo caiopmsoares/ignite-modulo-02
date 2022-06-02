c5ca2620a777464c0305d2d644daac77e91dbf69
Caio Macedo <caiopierre7@hotmail.com>
2022-06-02 02:43:37 -0300

    Adicionando os elementos do modal e estilzando-os

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,5 +1,8 @@
 import Modal from 'react-modal';
-import { Container } from './styles';
+import incomeImg from '../../assets/income.svg';
+import outcomeImg from '../../assets/outcome.svg';
+import closeImg from '../../assets/close.svg';
+import { Container, TransactionTypeContainer } from './styles';
 
 interface NewTransactionModalProps {
 	isOpen: boolean;
@@ -17,12 +20,31 @@ export function NewTransactionModal({
 			overlayClassName="react-modal-overlay"
 			className="react-modal-content"
 		>
+			<button type="button" onClick={onRequestClose} className="react-modal-close">
+				<img src={closeImg} alt="Fechar modal" />
+			</button>
+
 			<Container>
 				<h2>Cadastrar Transação</h2>
 
 				<input placeholder="Titulo" />
+
 				<input type="number" placeholder="Valor" />
+
+				<TransactionTypeContainer>
+					<button type="button">
+						<img src={incomeImg} alt="Entrada" />
+						<span>Entrada</span>
+					</button>
+
+					<button type="button">
+						<img src={outcomeImg} alt="Saída	" />
+						<span>Saída</span>
+					</button>
+				</TransactionTypeContainer>
+
 				<input placeholder="Categoria" />
+
 				<button type="submit"> Cadastrar </button>
 			</Container>
 		</Modal>
