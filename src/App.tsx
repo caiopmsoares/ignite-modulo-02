ece599577c54ef662130e1024519db8495470ea8
Caio Macedo <caiopierre7@hotmail.com>
2022-06-02 22:37:53 -0300

    Adicionando context react

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,10 @@ import { Header } from './components/Header';
 import { NewTransactionModal } from './components/NewTransactionModal';
 
 import { GlobalStyle } from './styles/global';
+import {
+	TransactionsContext,
+	TransactionsProvider,
+} from './TransactionsContext';
 
 Modal.setAppElement('#root');
 
@@ -22,14 +26,17 @@ export function App() {
 	}
 
 	return (
-		<>
+		<TransactionsProvider>
 			<Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
+
 			<Dashboard />
+
 			<NewTransactionModal
 				isOpen={isNewTransactionModalOpen}
 				onRequestClose={handleCloseNewTransactionModal}
 			/>
+
 			<GlobalStyle />
-		</>
+		</TransactionsProvider>
 	);
 }
